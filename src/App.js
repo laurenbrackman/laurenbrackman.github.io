32d867805f8d4d7d277aa12440d6a520de276c30
Lauren Helbling <laurengraceh97@gmail.com>
2024-03-16 22:16:48 -0600

    add footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { NavBar } from './components/NavBar';
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
-import { Contact } from "./components/Contact";
+import { About } from "./components/About";
 import { Footer } from "./components/Footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,6 +14,8 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <About />
+      <Footer />
     </div>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -24,21 +24,6 @@ export const Projects = () => {
       description: "Design & Development",
       imgUrl: projImg3,
     },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
   ];
 
   return (
