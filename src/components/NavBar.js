18d45061c8440626ecd74ce4a1429668e8d4a086
Lauren Helbling <laurengraceh97@gmail.com>
2024-03-16 20:45:30 -0600

    add nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,12 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useState, useEffect } from 'react';
+import logo from "../assets/logo.svg";
+import navIcon1 from "../assets/nav-icon1.svg";
+import navIcon2 from "../assets/nav-icon2.svg";
+import navIcon3 from "../assets/nav-icon3.svg";
 
-function NavBar() {
+export const NavBar = () => {
   const {activeLink, setActiveLink} = useState('home');
   const {scrolled, setScrolled} = useState(false);
 
@@ -30,10 +34,11 @@ function NavBar() {
     <Navbar expand="lg" className={scrolled ? "scrolled":""}>
       <Container>
         <Navbar.Brand href="#home">
-            <img src={''} alt="logo"/>
+            <img src={logo} alt="logo"/>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={()=>onUpdateActiveLink('home')}>Home</Nav.Link>
@@ -42,9 +47,9 @@ function NavBar() {
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-                <a href="#"><img src={""} alt=""/></a>
-                <a href="#"><img src={""} alt=""/></a>
-                <a href="#"><img src={""} alt=""/></a>
+                <a href="#"><img src={navIcon1} alt=""/></a>
+                <a href="#"><img src={navIcon2} alt=""/></a>
+                <a href="#"><img src={navIcon3} alt=""/></a>
             </div>
             <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
             </span>
@@ -53,5 +58,3 @@ function NavBar() {
     </Navbar>
   );
 }
-
-export default NavBar;
