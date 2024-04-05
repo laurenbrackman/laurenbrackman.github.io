efb9d9e4f67490878801f71c81f1d3858cff0d09
Lauren Brackman <lauren.g.brackman@gmail.com>
2024-04-05 09:59:18 -0600

    update about me

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ export const About = () => {
           <Col size={12} md={6}>
               <div className={"animate__animated animate__fadeIn"}>
                 <h2>Get To Know Me</h2>
-                <p>After graduating with a degree in computer science, I ventured into the world of graphic design as a full-time church media manager. When I’m not designing, I am coding in coffee shops, jogging with my husband, and sewing for my handmade crafting business.</p>
+                <p>After graduating with a degree in computer science from Montana State University, I ventured into graphic design as a full-time church media manager. At the intersection of coding and design, I love helping small businesses and Christian ministries develop effective websites. When not designing, I enjoy coding in coffee shops, jogging with my husband, and sewing for my handmade crafting business.</p>
               </div>
           </Col>
         </Row>
