c54e64573a9e946d71d181ee7eef28e772f8f889
Lauren Brackman <lauren.g.brackman@gmail.com>
2024-04-11 13:57:26 -0600

    Rearrange banner photo

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,14 +6,13 @@ export const Banner = () => {
     <section className="banner" id="home">
       <Container>
         <Row className="align-items-center">
+        <Col xs={12} md={6} xl={5}>
+             <img src={headerImg} alt="Headshot of Lauren Brackman"/>
+          </Col>
           <Col xs={12} md={6} xl={7}>
-                <span className="tagline">WELCOME WELCOME WELCOME</span>
                 <h1>{`Hi, I'm Lauren`}</h1>
                 <h3>I'm a Web Designer and Developer.</h3>
                 <h3>I love working with churches, small businesses, and startups.</h3>
-          </Col>
-          <Col xs={12} md={6} xl={5}>
-             <img src={headerImg} alt="Headshot of Lauren Brackman"/>
           </Col>
         </Row>
       </Container>
