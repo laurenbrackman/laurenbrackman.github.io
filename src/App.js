c4be7afd88e8c7e19fcf026c9670fb39129a51b0
Lauren Brackman <lauren.g.brackman@gmail.com>
2024-04-11 11:42:54 -0600

    Add css file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Banner } from "./components/Banner";
 import { Projects } from "./components/Projects";
 import { About } from "./components/About";
 import { Footer } from "./components/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
