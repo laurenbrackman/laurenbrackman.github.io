8c4394b3c9821104c5e863d8411d89324e00187e
Lauren Brackman <lauren.g.brackman@gmail.com>
2024-04-15 11:42:26 -0600

    Prototype work with me section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { NavBar } from './components/NavBar';
 import { Banner } from "./components/Banner";
 import { Projects } from "./components/Projects";
 import { About } from "./components/About";
+import { WorkWithMe } from './components/WorkWithMe';
 import { Footer } from "./components/Footer";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Banner />
       <Projects />
       <About />
+      <WorkWithMe />
       <Footer />
     </div>
   );
---
+++ src/components/WorkWithMe.js
@@ -0,0 +1,10 @@
+export const WorkWithMe = () => {
+    return (
+        <section className="workwithme">
+            <div className="content">
+                <h2>Work With Me</h2>
+                <p>Looking for professional web design and development services? Look no further! As a freelance web designer, I offer top-notch solutions tailored to your needs. Whether you're looking to kickstart your online presence with a FREE portfolio-building project or seeking to bolster your team with a skilled full-time web designer, I've got you covered. With expertise in crafting visually stunning and user-friendly websites, I'm committed to bringing your vision to life. Let's collaborate and create something remarkable together.</p>
+            </div>
+        </section>
+    )
+}
